9e7111a1795ef6577145142c124f046c1f14a328
Vbdiaz <vbdiaz03@gmail.com>
2024-02-22 15:06:20 -0600

    Home Page

---
+++ app/components/navigation/navbar/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+import Logo from "./Logo";
+import Button from "./Button";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
+            <Logo />
+            <ul>
+            </ul>
+            <div>
+              <Button />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
