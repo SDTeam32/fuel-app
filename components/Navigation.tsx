7b1889dc12eb701632acfab5e5f5753de3621961
Cruz Salas <cruz.danielss@icloud.com>
2024-04-09 15:08:03 -0500

    fixed modal styling

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -41,7 +41,7 @@ export default function Navigation() {
   
 
   return (
-    <nav className="bg-white dark:bg-gray-900 relative w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-white dark:bg-gray-900 relative w-full z-2 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="/">
           <div className="flex items-center space-x-3 rtl:space-x-reverse">
