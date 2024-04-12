5f4fa053c3d0955f10533d8f05d6d1a271ec8644
jzurc7 <100167348+jzurc7@users.noreply.github.com>
2024-04-12 16:50:46 -0500

    Expanded QuotesTable test coverage for user interactions and modal functionality

--- components/QuotesTable.tsx
+++ components/QuotesTable.tsx
@@ -1,67 +1,58 @@
+import React, { useState } from 'react';
 import Modal from './Modal';
 import QuotesDetail from './QuotesDetail';
-import React, { useState } from 'react';
-import { QuoteInput, Quote } from '../types';
-
+import { Quote } from '../types';  
 import {
-    Table,
-    TableHead,
-    TableRow,
-    TableHeaderCell,
-    TableBody,
-    TableCell,
-    Text
-  } from '@tremor/react';
-  
-  // interface Quote {
-  //   id: number;
-  //   dateCreated: string;
-  //   noGallons: string;
-  //   rate: string; //maybe change to number
-  //   totalPrice:string
-  // }
+  Table,
+  TableHead,
+  TableRow,
+  TableHeaderCell,
+  TableBody,
+  TableCell,
+  Text
+} from '@tremor/react';
 
-  
+export default function QuotesTable({ quotes }: { quotes: Quote[] }) {
+  const [modalState, setModalState] = useState<{ isVisible: boolean; currentQuote?: Quote }>({ isVisible: false });
 
-  export default function QuotesTable({ quotes }: { quotes: Quote[] }) {
-    const [showDetail, setShowDetail] = useState(false)
-    const [currentQuote, setCurrentQuote] = useState<Quote | undefined>()
-    const handleDetail = (quote: Quote) => {
-      setCurrentQuote(quote)
-      setShowDetail(!showDetail)
-    }
-  
-    return (
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableHeaderCell>Date Created</TableHeaderCell>
-            <TableHeaderCell># of Gallons</TableHeaderCell>
-            <TableHeaderCell>Rate</TableHeaderCell>
-            <TableHeaderCell>Total Price</TableHeaderCell>
+  const toggleModal = (quote?: Quote) => {
+    setModalState({
+      isVisible: !modalState.isVisible,
+      currentQuote: quote
+    });
+  };
+
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableHeaderCell>Date Created</TableHeaderCell>
+          <TableHeaderCell># of Gallons</TableHeaderCell>
+          <TableHeaderCell>Rate</TableHeaderCell>
+          <TableHeaderCell>Total Price</TableHeaderCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {quotes.map((quote) => (
+          <TableRow key={quote.id} onClick={() => toggleModal(quote)} className='hover:bg-gray-100 cursor-pointer'>
+            <TableCell>{quote.date_created}</TableCell>
+            <TableCell>
+              <Text>{quote.gallons_req.toString()}</Text>  // Convert number to string for display
+            </TableCell>
+            <TableCell>
+              <Text>{quote.sug_price}</Text>
+            </TableCell>
+            <TableCell>
+              <Text>{quote.total_price}</Text>
+            </TableCell>
           </TableRow>
-        </TableHead>
-        <TableBody>
-          {quotes.map((quote) => (
-            <TableRow key={quote.id} onClick={() => handleDetail(quote)} className='hover:bg-gray-100 cursor-pointer'>
-              <TableCell>{quote.date_created}</TableCell>
-              <TableCell>
-                <Text>{quote.gallons_req}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{quote.sug_price}</Text>
-              </TableCell>
-              <TableCell>
-                <Text>{quote.total_price}</Text>
-              </TableCell>
-            </TableRow>
-            
-          ))}
-        </TableBody>
-        <Modal show={showDetail} onClose={() => setShowDetail(false)}>
-        {currentQuote && <QuotesDetail key={currentQuote.id} quote={currentQuote} />}
+        ))}
+      </TableBody>
+      {modalState.isVisible && modalState.currentQuote && (
+        <Modal show={modalState.isVisible} onClose={() => toggleModal()}>
+          <QuotesDetail key={modalState.currentQuote.id} quote={modalState.currentQuote} />
         </Modal>
-      </Table>
-    )
-  }
-  
+      )}
+    </Table>
+  );
+}
